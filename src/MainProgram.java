ffb7ec47edd1a6e723fd5ebb1ae111ba6ce3692d
unknown <aitorsimon@deusto.es>
2015-01-20 00:25:10 +0100

    Nuevo

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,71 +1,111 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainProgram {
 	
+	static String [] prepositions={" a ", " ante ", " bajo ", " cabe ", " con ", " contra ", " de ", " desde ", 
+			" durante ", " en ", " entre ", " hacia ", " hasta ", " mediante ", " para ", " por ", 
+			" según ", " sin ", " so ", " sobre ", " tras ", " versus ", " vía ", "la ", " si "};		
+
+	static String [] determinantes = { " su ", " al ", " esta ", " ese " , " mi ", " la ", " el ", 
+					" lo ", " las ", " los " , " l@s ", " tus ", " ellos ", " ellas ", 
+					" tu ", " un ", " una ", " del ", " y ", " o ", " nuestras ", " nuestros ", " cualquier ", " nuestra", " cada "};
+	
+	static String [] pronombres = { " me ", " te ", " se ", " nos ", " os " };
+	
+	static String [] signosDePuntacion = { ";", "¿", ": ", " ? ", " : ", " ¿ ", "\\? ", " +"};
+	
+	static String [] diasDeLaSemana = { " lunes ", " martes ", " miercoles ", " jueves ", " viernes ", " sabado ", " domingo " };
+	
+	static String [] stopWords = { " unas ", " unos ", " un ", " del ", " al "," de ", " en ", " sobre ", " por ", " dentro ", " hacia ", " desde ", " fuera ", " como ", " así ", " tal ", " esos ", " esas ", " este ", " aquellas ", " aquellos ", " esa ",
+	" para ", " es ", " fue ", " era ",  " soy ", " eres ", " sido ", " eras " };
+
 	public static void main(String[] args) {
 		
 		ReadCSV reader=new ReadCSV();
 		List<Tuit>publicidad=reader.run("Publicidad.csv");
 		List<Tuit>noPublicidad=reader.run("NoPublicidad.csv");
 		
-		String [] prepositions={" a ", " ante ", " bajo ", " cabe ", " con ", " contra ", " de ", " desde ", 
-								" durante ", " en ", " entre ", " hacia ", " hasta ", " mediante ", " para ", " por ", 
-								" según ", " sin ", " so ", " sobre ", " tras ", " versus ", " vía ", "la ", " si "};		
-		
-		String [] determinantes = { " su ", " al ", " esta ", " ese " , " mi ", " la ", " el ", 
-									" lo ", " las ", " los " , " l@s ", " tus ", " ellos ", " ellas ", 
-									" tu ", " un ", " una ", " del ", " y ", " o ", " nuestras ", " nuestros ", " cualquier ", " nuestra", " cada "};
-		
-		String [] pronombres = { " me ", " te ", " se ", " nos ", " os " };
-		
-		String [] signosDePuntacion = { ";", "¿", ": ", " ? ", " : ", " ¿ ", "\\? ", " +"};
-		
-		String [] diasDeLaSemana = { " lunes ", " martes ", " miercoles ", " jueves ", " viernes " };
-		
-		for(Tuit tuit: publicidad){
-			reemplazarValores ( tuit, prepositions );
-			reemplazarValores(tuit, determinantes );
-			reemplazarValores(tuit, signosDePuntacion);
-			reemplazarValores(tuit, pronombres);
-			reemplazarValores(tuit, diasDeLaSemana);
-			System.out.println(tuit.getTexto());
-		}
-		
-		for(Tuit tuit: noPublicidad){
-			reemplazarValores ( tuit, prepositions );
-			reemplazarValores(tuit, determinantes );
-			reemplazarValores(tuit, signosDePuntacion);
-			reemplazarValores(tuit, pronombres);
-			reemplazarValores(tuit, diasDeLaSemana);
-		}
-		
+		reemplazarValores ( publicidad );
+		reemplazarValores( noPublicidad );
+
 		for ( Tuit tuit : publicidad )
 		{
 			obtenerNumeroDeCorrespondencias( tuit );
 		}
+		
+		for ( Tuit tuit : noPublicidad )
+		{
+			obtenerNumeroDeCorrespondencias( tuit );
+		}
+		
 		getNumberOfUrls(publicidad);
-		//getNumberOfUrls(noPublicidad);
+		getNumberOfUrls(noPublicidad);
+		
 		getNumberOfMentions(publicidad);
-		//getNumberOfMentions(noPublicidad);
+		getNumberOfMentions(noPublicidad);
+		
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("FinalText.txt", "UTF-8");
+			escribirEnFichero(writer, publicidad, true );
+			escribirEnFichero(writer, noPublicidad, false );
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
 	}
 	
+	public static void escribirEnFichero ( PrintWriter writer, List<Tuit> tuits, boolean publicidad )
+	{
+		for ( Tuit tuit : tuits )
+		{
+			String linea = "";
+			for ( String keyWord : tuit.getHashMap().keySet() )
+			{
+				linea += tuit.getHashMap().get(keyWord) + ",";
+			}
+			linea += tuit.getNumberOfMentions() + ",";
+			linea += tuit.getNumberOfURLs() + ",";
+			if ( publicidad )
+				linea += "1";
+			else
+				linea += "0";
+			writer.println(linea);
+		}
+	}
+	
+	public static void reemplazarValores ( List<Tuit> tuits )
+	{
+		for ( Tuit tuit : tuits )
+		{
+			reemplazarValores ( tuit, prepositions );
+			reemplazarValores(tuit, determinantes );
+			reemplazarValores(tuit, pronombres);
+			reemplazarValores(tuit, signosDePuntacion);
+			reemplazarValores(tuit, diasDeLaSemana);
+			reemplazarValores(tuit, stopWords);
+		}
+	}
 	public static void obtenerNumeroDeCorrespondencias ( Tuit tuit )
 	{
-		System.out.println(tuit.getTexto());
 		String [] palabras = tuit.getTexto().split(" ");
 		for ( String palabra : palabras )
 		{
-			if ( Utilidades.correspondencia("regalo", palabra) )
+			for ( String key : tuit.getHashMap().keySet() )
 			{
-				System.out.println("Correspondencia 1");
+				if ( Utilidades.correspondencia(key, palabra) )
+				{
+					tuit.getHashMap().put ( key, tuit.getHashMap().get(key) + 1 );	
+				}
 			}
-			if ( Utilidades.correspondencia("compra", palabra) )
-			{
-				System.out.println("Correspondencia 2");
-			}
-			
 		}
 	}
 	
--- src/Tuit.java
+++ src/Tuit.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Tuit {
 	private String texto;
 	private int seguidores;
@@ -5,6 +7,35 @@ public class Tuit {
 	private int tweets;
 	private int numberOfURLs;
 	private int numberOfMentions;
+	private HashMap<String,Integer> hashMap;
+	
+	public static String descuento = "descuent";
+	public static String oferta = "ofert";
+	public static String compra = "compr";
+	public static String producto = "product";
+	public static String PRUEBA = "prueb";
+	public static String REBAJA = "rebaj";
+	public static String PORCENTAJE = "%";
+	public static String PERSONA = "person";
+	public static String VIDEOJUEGO = "videojueg";
+	public static String AMOR = "amor";
+	public static String AGRADECER = "agradec";
+	public static String DTO = "dto";
+	public static String NUEVO = "nuev";
+	public static String REGALO = "regal";
+	public static String BUSCAR = "busc";
+	public static String AHORRO = "ahorr";
+	public static String NAVIDAD = "navid";
+	public static String ADQUIERE = "adquier";
+	public static String FORMA = "form";
+	public static String NO = "no";
+	public static String CUANDO = "cuand";
+	public static String QUERER = "quer";
+	public static String ALGO = "algo";
+	public static String MUY = "muy";
+	public static String TODO = "tod";
+	public static String EURO = "€";
+	public static String SEGUIR = "segui";
 
 	public Tuit(String texto, int seguidores, int siguiendo, int tweets) {
 		this.texto = texto;
@@ -13,6 +44,34 @@ public class Tuit {
 		this.tweets = tweets;
 		this.numberOfMentions=0;
 		this.numberOfURLs=0;
+		
+		hashMap = new HashMap<String, Integer>();
+		hashMap.put(descuento, 0 );
+		hashMap.put(oferta, 0 );
+		hashMap.put(compra, 0 );
+		hashMap.put(producto, 0 );
+		hashMap.put(PRUEBA, 0 );
+		hashMap.put(REBAJA, 0 );
+		hashMap.put(PORCENTAJE, 0 );
+		hashMap.put(PERSONA, 0 );
+		hashMap.put(VIDEOJUEGO, 0 );
+		hashMap.put(AMOR, 0 );
+		hashMap.put(AGRADECER, 0 );
+		hashMap.put(DTO, 0 );
+		hashMap.put(NUEVO, 0 );
+		hashMap.put(REGALO, 0 );
+		hashMap.put(BUSCAR, 0 );
+		hashMap.put(AHORRO, 0 );
+		hashMap.put(NAVIDAD, 0 );
+		hashMap.put(ADQUIERE, 0 );
+		hashMap.put(FORMA, 0 );
+		hashMap.put(NO, 0 );
+		hashMap.put(CUANDO, 0 );
+		hashMap.put(QUERER, 0 );
+		hashMap.put(ALGO, 0 );
+		hashMap.put(MUY, 0 );
+		hashMap.put(TODO, 0 );
+		hashMap.put(EURO, 0);
 	}
 
 	public String getTexto() {
@@ -65,6 +124,14 @@ public class Tuit {
 
 	public String toString(){
 		return "Texto: "+texto+" Seguidores: "+seguidores+" Siguiendo: "+siguiendo+" Tweets: "+tweets ;
+	}
+
+	public HashMap<String, Integer> getHashMap() {
+		return hashMap;
+	}
+
+	public void setHashMap(HashMap<String, Integer> hashMap) {
+		this.hashMap = hashMap;
 	}
 
 }
